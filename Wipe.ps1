09b6ba03c30d7bed1519054d55a4768308c2c666
Powershell Lover <151552809+EvilBytecode@users.noreply.github.com>
2024-02-03 23:02:00 +0000

    Create Wipe.ps1

---
+++ Wipe.ps1
@@ -0,0 +1,33 @@
+if (-not ([Security.Principal.WindowsPrincipal] [System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$($MyInvocation.MyCommand.Path)`"" -Verb RunAs
+    exit
+}
+
+function Get-CelexPrefetch {
+    $celexprefetcj = Get-ChildItem -Path "C:\Windows\Prefetch" -Filter "*CELEX.EXE*.pf"
+
+    if ($celexprefetcj) {
+        $celexprefetcj | ForEach-Object {
+            Write-Host -ForegroundColor Green "[+] Found in Prefetch: Celex Trace Deleting It : $($_.FullName)" 
+            Remove-Item $_.FullName -Force
+        }
+    } else {
+        Write-Host -ForegroundColor Red "[-] No Celex Trace in Prefetch."
+    }
+}
+
+function CelexAppData {
+    $celexappdat = "C:\Users\$env:USERNAME\AppData\Roaming\celex-v2"
+
+    if (Test-Path $celexappdat) {
+        Write-Host -ForegroundColor Green "[+] Detected: celex-v2 folder. Deleting..."
+        Remove-Item $celexappdat -Recurse -Force
+    } else {
+        Write-Host -ForegroundColor Red "[-] celex-v2 folder not found."
+    }
+}
+
+Get-CelexPrefetch
+CelexAppData
+
+Start-Sleep -Seconds 10
